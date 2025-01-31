45f55006e0c291b5f73b394e819ecec9c9b8d6e9
33sahis3 <151052221+33sahis3@users.noreply.github.com>
2025-01-31 18:23:02 +0300

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -3,10 +3,10 @@ window.onscroll = function() {
     var header = document.querySelector("header");
     var nav = document.querySelector("nav");
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        header.style.top = "-55px"; // Başlık kaybolur
-        nav.style.top = "10px"; // Menü yukarı kayar
+        header.style.top = "-45px"; // Başlık kaybolur
+        nav.style.top = "0px"; // Menü yukarı kayar
     } else {
         header.style.top = "0"; // Başlık geri gelir
-        nav.style.top = "70px"; // Menü geri eski konumuna döner
+        nav.style.top = "45px"; // Menü geri eski konumuna döner
     }
 };
